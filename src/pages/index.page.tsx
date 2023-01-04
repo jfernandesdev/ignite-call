fcce7b82ef0f60f2a322d5d6e8c4281a5d15b492
jfernandesdev <jfernandes.dev@gmail.com>
2023-01-04 14:13:04 -0300

    CHORE: setup and initial structure

---
+++ src/pages/index.page.tsx
@@ -0,0 +1,15 @@
+import { Heading, Text } from '@ignite-ui/react'
+
+export default function Home() {
+  return (
+    <>
+      <Heading as="h1">Hello World!</Heading>
+      <Text>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam
+        consequuntur a, unde veniam similique sapiente rem praesentium
+        voluptates odit id tempora quae, omnis nisi iste culpa nulla atque eaque
+        cumque?
+      </Text>
+    </>
+  )
+}
