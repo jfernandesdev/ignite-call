25123d2bad379d6ca759d2b0b2f9100161543ba0
jfernandesdev <jfernandes.dev@gmail.com>
2023-01-16 11:55:03 -0300

    FEAT: route, loading and display of available times

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,4 +12,18 @@ export const globalStyles = globalCss({
     color: '$gray100',
     '-webkit-font-smoothing': 'antialiased',
   },
+
+  '::-webkit-scrollbar': {
+    width: '6px',
+    height: 0,
+  },
+
+  '::-webkit-scrollbar, ::-webkit-scrollbar-track': {
+    background: 'transparent',
+  },
+
+  '::-webkit-scrollbar-thumb': {
+    background: '$gray500',
+    borderRadius: '6px',
+  },
 })
